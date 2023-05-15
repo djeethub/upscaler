fb7272fdb605a3c373c9277ba30e24a8306a7da9
Eric Summer <lcianr@gmail.com>
2023-05-16 01:30:17 +0900

    t

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='upscaler',
-    version='0.0.18',
+    version='0.0.19',
     description='upscaler',
     url='https://github.com/djeethub/upscaler.git',
     packages=find_packages(),
--- upscaler/__init__.py
+++ upscaler/__init__.py
@@ -1,2 +1,1 @@
-from . import swin2sr
 from .main import get_upscaler
